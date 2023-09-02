358b80ecfdc174b7dac9e1cd32dfa20baf19b707
Pedro Lucas Macedo de Moraes <73931509+Null-Kc@users.noreply.github.com>
2023-09-02 04:50:11 -0300

    update Scrolling

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -7,7 +7,7 @@ function CardProject({ item, setSelectedId }) {
   return (
     <motion.div key={item.id} layoutId={item.id} onClick={() => setSelectedId(item.id)} className="w-full h-[400px] shadow-lg rounded-lg cursor-pointer transition-transform transform hover:scale-105 bg-white">
       <div className='rounded-lg h-2/4'>
-        <iframe title={item.title} scrolling="no" width="560" height="315" className='overflow-hidden w-full h-full rounded-lg' src={item.linkProjeto}></iframe >
+        <iframe title={item.title} scrolling="auto" width="560" height="315" className='w-full h-full rounded-lg' src={item.linkProjeto}></iframe >
       </div>
       <div className='py-4 h-2/4 flex text-center justify-center flex-col w-full'>
         <h5 className="text-[#461a1a]">{item.subtitle}</h5>
